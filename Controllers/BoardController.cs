3efb9905814322faff3ca22997fde166147c10fb
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-11 17:15:05 +0900

    보드컨트롤러 생성

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class BoardController : Controller
+    {
+        // GET: Board
+        public string List(int? Id)
+        {
+            if (Id == null)
+                return "Error Message Board";
+            return "보드리스트: " + Id.Value;
+        }
+    }
+}
