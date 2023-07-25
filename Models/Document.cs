975c263407ee3f4740a729d158e15962a1b08e57
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-25 14:26:15 +0900

    Models Document 생성

---
+++ Models/Document.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class Document
+    {
+        public int Document_Number { get; set; }
+        public string Title { get; set; }
+        public string Writer { get; set; }
+    }
+}
